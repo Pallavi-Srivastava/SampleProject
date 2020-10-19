7207c1e4b146289efa80d820ac3af62b4bebcc64
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-19 23:45:09 +0530

    HelloWorld Sample Project with maven

---
+++ HelloWorld/src/main/java/com/btm/helloworld/App.java
@@ -0,0 +1,20 @@
+package com.btm.helloworld;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App 
+{
+	 private static final Logger LOG = LogManager.getLogger(App.class);
+	    public static void main( String[] args )
+	    {
+	        String message="Hello world!";
+	        LOG.debug(message + " Will Be Printed on Debug");
+	        LOG.info(message + " Will Be Printed on Info");
+	        LOG.warn(message + " Will Be Printed on warn");
+	        LOG.error(message + " Will Be Printed on Error");
+	        LOG.fatal(message + " Will Be Printed on fatal");
+	        LOG.info("Appending string: {}.",message);
+	        System.out.println(message);
+	    }
+}
